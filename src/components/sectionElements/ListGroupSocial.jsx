55b8274e73f4d7c18ebd534509b20cdd3c172378
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-05 08:59:07 -0300

    fix: Ajeita imagem na Hero

--- src/components/sectionElements/ListGroupSocial.jsx
+++ src/components/sectionElements/ListGroupSocial.jsx
@@ -32,7 +32,7 @@ export default function ListGroupSocial({ LightMode }) {
 
   return (
     <ul
-      className={`h-14 hidden desktop1:flex my-auto items-center justify-end tablet1:items-center desktop1:gap-8 desktop2:gap-8 w-full font-normal text-paragraph3 font-secondFont ${
+      className={`h-14 hidden w-auto desktop1:flex items-center justify-end tablet1:items-center desktop1:gap-8 desktop2:gap-8  font-normal text-paragraph3 font-secondFont ${
         LightMode
           ? scrolling
             ? "text-black"
